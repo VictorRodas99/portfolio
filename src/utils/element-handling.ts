8ced46ba245f538c8238036836d403e6bfcabaff
Victor Rodas <victormatiasrodas100@gmail.com>
2023-08-21 12:41:02 -0400

    add new function to clone elements

--- src/utils/element-handling.ts
+++ src/utils/element-handling.ts
@@ -1,4 +1,10 @@
-export function getAllDescendants({ parent }: { parent: HTMLElement }): Element[] {
+import { themeButtonDescendants } from '@client-scripts/dialog-theme-controller'
+
+export function getAllDescendants({
+  parent
+}: {
+  parent: HTMLElement | Element
+}): Element[] {
   const allDescendants = []
 
   if (!parent) {
@@ -15,4 +21,25 @@ export function getAllDescendants({ parent }: { parent: HTMLElement }): Element[
   getChildren(parent)
 
   return allDescendants
+}
+
+export function getClonedElementFrom(
+  parent: HTMLElement,
+  selector: string,
+  deepClone = true
+) {
+  if (typeof selector !== 'string') {
+    throw new Error('selector is not a string')
+  }
+
+  return parent.querySelector(selector).cloneNode(deepClone)
+}
+
+export function changeThemeButtonIcon(newIcon: Node) {
+  const themeButton = document.querySelector('#theme-button')
+
+  themeButton.removeChild(themeButton.querySelector('svg')) // remove current svg icon
+  themeButton.appendChild(newIcon)
+
+  themeButtonDescendants.set(getAllDescendants({ parent: themeButton }))
 }
