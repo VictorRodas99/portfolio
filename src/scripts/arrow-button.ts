ee5d167fe256c4e40e3e69670b07bb325a06663c
Victor Rodas <victormatiasrodas100@gmail.com>
2023-08-31 13:51:59 -0400

    add functionality to the arrow button

---
+++ src/scripts/arrow-button.ts
@@ -0,0 +1,14 @@
+  const gotoButton = document.querySelector('#arrow-btn')
+  const sectionTitle = document.querySelector('#experience-section-title') as HTMLHeadingElement
+
+  if(!(gotoButton instanceof HTMLButtonElement)) {
+    throw new Error('#arrow-btn button does not exists')
+  }
+
+  if(!sectionTitle) {
+    throw new Error('Section title does not exists')
+  }
+
+  gotoButton.addEventListener('click', () => {
+    window.scrollTo({ top: sectionTitle.offsetTop - 120 })
+  })
