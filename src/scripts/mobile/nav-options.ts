cfafc85f9a80473c1c173ff66b4e3b5579e38906
Victor Rodas <victormatiasrodas100@gmail.com>
2023-09-02 01:33:13 -0400

    wrap all the script into a function

--- src/scripts/mobile/nav-options.ts
+++ src/scripts/mobile/nav-options.ts
@@ -1,22 +1,24 @@
-const arrowIcon = document.querySelector('#arrow-menu')
-const mobileMenuTrigger = document.querySelector('#mobile-menu-trigger') as HTMLDivElement
-const navOptionsDialog = document.querySelector('#nav-options-mobile') as HTMLDialogElement
-const closeButton = document.querySelector('#nav-options__close-btn') as HTMLButtonElement
+export default function MobileNavController() {
+  const arrowIcon = document.querySelector('#arrow-menu')
+  const mobileMenuTrigger = document.querySelector('#mobile-menu-trigger') as HTMLDivElement
+  const navOptionsDialog = document.querySelector('#nav-options-mobile') as HTMLDialogElement
+  const closeButton = document.querySelector('#nav-options__close-btn') as HTMLButtonElement
 
-if(!(closeButton instanceof HTMLButtonElement)) {
-  throw new Error('"#nav-options__close-btn" button does not exists')
+  if(!(closeButton instanceof HTMLButtonElement)) {
+    throw new Error('"#nav-options__close-btn" button does not exists')
+  }
+
+  if(!(navOptionsDialog instanceof HTMLDialogElement)) {
+    throw new Error('"#nav-options-mobile" dialog element does not exists')
+  }
+
+  mobileMenuTrigger.addEventListener('click', () => {
+    navOptionsDialog.showModal()
+    arrowIcon.classList.add('rotate-180')
+  })
+
+  closeButton.addEventListener('click', () => {
+    navOptionsDialog.close()
+    arrowIcon.classList.remove('rotate-180')
+  })
 }
-
-if(!(navOptionsDialog instanceof HTMLDialogElement)) {
-  throw new Error('"#nav-options-mobile" dialog element does not exists')
-}
-
-mobileMenuTrigger.addEventListener('click', () => {
-  navOptionsDialog.showModal()
-  arrowIcon.classList.add('rotate-180')
-})
-
-closeButton.addEventListener('click', () => {
-  navOptionsDialog.close()
-  arrowIcon.classList.remove('rotate-180')
-})
