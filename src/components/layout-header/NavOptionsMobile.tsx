8f92d0d7b6520c9068dcea2e9bdbd9b55ecde682
Victor Rodas <victormatiasrodas100@gmail.com>
2023-09-05 10:12:21 -0400

    modify component + fix click handler error

--- src/components/layout-header/NavOptionsMobile.tsx
+++ src/components/layout-header/NavOptionsMobile.tsx
@@ -1,25 +1,45 @@
+import ArrowDownIcon from '@components/icons/ArrowDownIcon'
 import CloseIcon from '@components/icons/CloseIcon.tsx'
 import NavList from '@components/layout-header/NavList.tsx'
-import MobileNavController from '@client-scripts/mobile/nav-options'
-import { useEffect } from 'react'
+import { useRef } from 'react'
 
 // TODO: animate dialog
 export default function NavOptionsMobile() {
-  useEffect(() => {
-    MobileNavController()
-  }, [])
+  const arrowIcon = useRef<SVGSVGElement>()
+  const navOptionsDialog = useRef<HTMLDialogElement>()
+
+  const dialogClickHandler = () => {
+    navOptionsDialog.current.showModal()
+    arrowIcon.current.classList.add('rotate-180')
+  }
+
+  const closeBtnClickHandler = () => {
+    navOptionsDialog.current.close()
+    arrowIcon.current.classList.remove('rotate-180')
+  }
 
   return (
-    <dialog id="nav-options-mobile" className="sm:hidden bg-primary-200 dark:bg-secondary-100 dark:text-primary-100 rounded-xl border-[1px] w-full p-7">
-      <header className="flex justify-between mb-10">
-        <h2>Navegación</h2>
-        <button id="nav-options__close-btn">
-          <CloseIcon />
-        </button>
-      </header>
-      <ul className="flex flex-col gap-5 [&>li]:border-b-[1px] dark:[&>li]:border-secondary-50 [&>li]:pb-3">
-        <NavList />
-      </ul>
-    </dialog>
+    <>
+      <button
+        onClick={dialogClickHandler}
+        id="mobile-menu-trigger"
+        className="px-3 p-2 flex gap-1 justify-center items-center sm:hidden"
+      >
+        Menú  
+        <ArrowDownIcon id="arrow-menu" ref={arrowIcon} /> 
+      </button>
+
+      <dialog id="nav-options-mobile" ref={navOptionsDialog} className=" bg-primary-200 dark:bg-secondary-100 dark:text-primary-100 rounded-xl border-[1px] w-full p-7">
+        <header className="flex justify-between mb-10">
+          <h2>Navegación</h2>
+          <button onClick={closeBtnClickHandler} id="nav-options__close-btn">
+            <CloseIcon />
+          </button>
+        </header>
+        <ul className="flex flex-col gap-5 [&>li]:border-b-[1px] dark:[&>li]:border-secondary-50 [&>li]:pb-3">
+          <NavList />
+        </ul>
+      </dialog>
+    </>
   )
 }
