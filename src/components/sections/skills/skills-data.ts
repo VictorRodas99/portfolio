1e7b4e4cc70bb5d49a31ea2838b4448453355414
Victor Rodas <victormatiasrodas100@gmail.com>
2023-09-08 18:23:00 -0400

    extract data in object

---
+++ src/components/sections/skills/skills-data.ts
@@ -0,0 +1,38 @@
+export default {  
+  react: {
+    name: 'React',
+    description: 'Una librería de JavaScript para construir interfaces de usuario.'
+  },
+  astro: {
+    name: 'Astro',
+    description: 'Un framework web todo en uno para crear páginas web rápidas y centradas en el contenido.'
+  },
+  typescript: {
+    name: 'TypeScript',
+    description: 'Un lenguaje de programación fuertemente tipado basado en JavaScript, que brinda mejores herramientas a cualquier escala.'
+  },
+  tailwind: {
+    name: 'Tailwind',
+    description: 'Un framework de CSS diseñado para crear cualquier tipo de diseño directamente en el HTML.'
+  },
+  bootstrap: {
+    name: 'Bootstrap',
+    description: 'El framework de CSS más popular para crear sitios responsivos y orientados a dispositivos móviles.'
+  },
+  nodejs: {
+    name: 'NodeJS',
+    description: 'Un entorno de ejecución para JavaScript.'
+  },
+  express: {
+    name: 'Express',
+    description: 'Un framework web customizable y rápido para NodeJS.'
+  },
+  python: {
+    name: 'Python',
+    description: 'Un lenguaje de programación ampliamente utilizado en aplicaciones web y el desarrollo de software en general, debido a su eficiencia, versatilidad y capacidad de acelerar el desarrollo de software.'
+  },
+  mysql: {
+    name: 'MySLQ',
+    description: 'Sistema de gestión de base de datos relacionales basado en SQL.'
+  }
+}
