d596cc1b7ccd511e98e602d392d46319316d7f02
Victor Rodas <victormatiasrodas100@gmail.com>
2023-09-09 13:17:34 -0400

    add click handlers for navigation options

--- src/components/sections/layout-header/NavList.tsx
+++ src/components/sections/layout-header/NavList.tsx
@@ -1,17 +1,31 @@
+import { notification } from '@utils/development-notification'
+
 export default function NavList({ className }: { className?: string }) {
+  const scrollToSection = (targetSelector: string) => {
+    const targetElement = document.querySelector(targetSelector) as HTMLElement
+
+    window.scrollTo({
+      top: targetElement.offsetTop
+    })
+  }
+
+  const showDevNotification = () => {
+    notification.showToast()
+  }
+
   return (
     <>
       <li className={className}>
         <a href="/">Inicio</a>
       </li>
       <li className={className}>
-        <a href="#">Acerca de</a>
+        <a href="/about">Acerca de</a>
       </li>
-      <li className={className}>
-        <a href="#">Proyectos</a>
+      <li className={className} onClick={() => scrollToSection('#projects-section-title')}>
+        Proyectos
       </li>
-      <li className={className}>
-        <a href="#">Contacto</a>
+      <li className={className} onClick={showDevNotification}>
+        Contacto
       </li>
     </>
   )
