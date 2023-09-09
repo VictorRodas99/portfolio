a155586fd9c6e6b3ba795a19099ad473a1f1eb0b
Victor Rodas <victormatiasrodas100@gmail.com>
2023-09-09 15:25:18 -0400

    remove unnecessary handler

--- src/components/sections/layout-header/NavList.tsx
+++ src/components/sections/layout-header/NavList.tsx
@@ -1,14 +1,6 @@
 import { notification } from '@utils/development-notification'
 
 export default function NavList({ className }: { className?: string }) {
-  const scrollToSection = (targetSelector: string) => {
-    const targetElement = document.querySelector(targetSelector) as HTMLElement
-
-    window.scrollTo({
-      top: targetElement.offsetTop
-    })
-  }
-
   const showDevNotification = () => {
     notification.showToast()
   }
@@ -21,8 +13,8 @@ export default function NavList({ className }: { className?: string }) {
       <li className={className}>
         <a href="/about">Acerca de</a>
       </li>
-      <li className={className} onClick={() => scrollToSection('#projects-section-title')}>
-        Proyectos
+      <li className={className}>
+        <a href="/#projects">Proyectos</a>
       </li>
       <li className={className} onClick={showDevNotification}>
         Contacto
