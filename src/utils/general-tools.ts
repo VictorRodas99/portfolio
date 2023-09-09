0d8fa700d74833f2bfc97f9c8d7bac6fafa1c8bd
Victor Rodas <victormatiasrodas100@gmail.com>
2023-09-09 11:49:30 -0400

    add random generators

--- src/utils/general-tools.ts
+++ src/utils/general-tools.ts
@@ -1,3 +1,12 @@
+export const getRandomNumber = (to: number) => Math.ceil(Math.random() * to)
+
+export const getRandomRGB = () => {
+  const red = getRandomNumber(255)
+  const green = getRandomNumber(255)
+
+  return `rgb(${red}, ${green}, 226)`
+}
+
 /**
  * Normalizes a hexadecimal color code into an array of normalized RGB values
  */
