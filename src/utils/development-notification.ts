335f72da8a6707d20408ff4fcb53d0d637f313ff
Victor Rodas <victormatiasrodas100@gmail.com>
2023-09-10 11:29:01 -0400

    format code

--- src/utils/development-notification.ts
+++ src/utils/development-notification.ts
@@ -2,14 +2,13 @@ import Toastify from 'toastify-js'
 import 'toastify-js/src/toastify.css'
 
 export const notification = Toastify({
-    text: 'Aún en desarollo 🔨',
-    duration: 2000,
-    position: 'right',
-    gravity: 'bottom',
-    style: {
-      font: 'bold',
-      background: '#f8e91d',
-      color: '#000'
-    }
+  text: 'Aún en desarollo 🔨',
+  duration: 2000,
+  position: 'right',
+  gravity: 'bottom',
+  style: {
+    font: 'bold',
+    background: '#f8e91d',
+    color: '#000'
   }
-)
+})
