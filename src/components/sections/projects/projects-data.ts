d8cfa9d6061785678c2c8877501dad3d6375f8fe
Victor Rodas <victormatiasrodas100@gmail.com>
2023-09-10 10:16:49 -0400

    add repo links

--- src/components/sections/projects/projects-data.ts
+++ src/components/sections/projects/projects-data.ts
@@ -2,11 +2,13 @@ export default {
   one: {
     name: 'E-commerce Full-Stack',
     description:
-      'Desarollado con ReactJS para el Front-End y HonoJS con edge workers para el Back-End. Aunque no permite realizar compras reales, la aplicación te mantendrá al día con los últimos productos de informática y electrónica. El catálogo se actualiza de manera automática semanalmente.'
+      'Desarollado con ReactJS para el Front-End y HonoJS con edge workers para el Back-End. Aunque no permite realizar compras reales, la aplicación te mantendrá al día con los últimos productos de informática y electrónica. El catálogo se actualiza de manera automática semanalmente.',
+    repo: 'https://github.com/VictorRodas99/ecommerce-web-project'
   },
   two: {
     name: 'just-music',
     description:
-      'Reproductor multimedia de línea de comandos escrito en NodeJS que admite la reproducción de contenido utilizando diferentes estrategias. La aplicación permite a los usuarios buscar y reproducir música de YouTube (ya sea playlists, o a través de una URL) sin la necesidad de utilizar un navegador.'
+      'Reproductor multimedia de línea de comandos escrito en NodeJS que admite la reproducción de contenido utilizando diferentes estrategias. La aplicación permite a los usuarios buscar y reproducir música de YouTube (ya sea playlists, o a través de una URL) sin la necesidad de utilizar un navegador.',
+    repo: 'https://github.com/VictorRodas99/just-music'
   }
 }
