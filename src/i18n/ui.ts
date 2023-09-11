efe97653f0409b39f95e48c16734cffef098bd4e
Victor Rodas <victormatiasrodas100@gmail.com>
2023-09-11 15:41:43 -0400

    add ui strings translation

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+  en: 'English',
+  es: 'Español'
+}
+
+export const defaultLang = 'es'
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.projects': 'Projects',
+    'nav.contact': 'Contact'
+  },
+  es: {
+    'nav.home': 'Inicio',
+    'nav.about': 'Acerca de',
+    'nav.projects': 'Proyectos',
+    'nav.contact': 'Contacto'
+  }
+}
