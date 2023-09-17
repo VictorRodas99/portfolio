0be35c283d9b300a13737f043dd4613cbfe9d185
Victor Rodas <victormatiasrodas100@gmail.com>
2023-09-16 20:33:27 -0400

    add animations

--- src/components/sections/layout-header/NavOptionsMobile.tsx
+++ src/components/sections/layout-header/NavOptionsMobile.tsx
@@ -1,21 +1,22 @@
 import NavList from '@components/sections/layout-header/NavList.tsx'
 import ArrowDownIcon from '@components/icons/ArrowDownIcon'
 import CloseIcon from '@components/icons/CloseIcon.tsx'
-import { useRef } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useRef, useState } from 'react'
 
-// TODO: animate dialog
 export default function NavOptionsMobile() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   const arrowIcon = useRef<SVGSVGElement>()
-  const navOptionsDialog = useRef<HTMLDialogElement>()
+  const navOptionsDialog = useRef<HTMLDivElement>()
 
   const dialogClickHandler = () => {
-    navOptionsDialog.current.showModal()
     arrowIcon.current.classList.add('rotate-180')
+    setModalIsOpen(true)
   }
 
   const closeBtnClickHandler = () => {
-    navOptionsDialog.current.close()
     arrowIcon.current.classList.remove('rotate-180')
+    setModalIsOpen(false)
   }
 
   return (
@@ -29,21 +30,35 @@ export default function NavOptionsMobile() {
         <ArrowDownIcon id="arrow-menu" ref={arrowIcon} />
       </button>
 
-      <dialog
-        id="nav-options-mobile"
-        ref={navOptionsDialog}
-        className=" bg-primary-200 dark:bg-secondary-100 dark:text-primary-100 rounded-xl border-[1px] w-full p-7"
-      >
-        <header className="flex justify-between mb-10">
-          <h2>Navegación</h2>
-          <button onClick={closeBtnClickHandler} id="nav-options__close-btn">
-            <CloseIcon />
-          </button>
-        </header>
-        <ul className="flex flex-col gap-5 [&>li]:border-b-[1px] dark:[&>li]:border-secondary-50 [&>li]:pb-3">
-          <NavList closeDialog={closeBtnClickHandler} />
-        </ul>
-      </dialog>
+      {modalIsOpen && (
+        <>
+          <div className='fixed right-0 top-0 z-20 w-screen h-screen bg-secondary-50/50'></div>
+          <AnimatePresence>
+            <motion.div
+              layout
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 20 }}
+              transition={{ ease: 'linear', duration: 0.3 }}
+              ref={navOptionsDialog}
+              className="fixed left-[10%] top-[15%] w-[80%] z-30 bg-primary-200 dark:bg-secondary-100 dark:text-primary-100 rounded-xl p-7"
+            >
+              <header className="flex justify-between mb-10">
+                <h2>Navegación</h2>
+                <button
+                  onClick={closeBtnClickHandler}
+                  id="nav-options__close-btn"
+                >
+                  <CloseIcon />
+                </button>
+              </header>
+              <ul className="flex flex-col gap-5 [&>li]:border-b-[1px] dark:[&>li]:border-secondary-50 [&>li]:pb-3">
+                <NavList closeDialog={closeBtnClickHandler} />
+              </ul>
+            </motion.div>
+          </AnimatePresence>
+        </>
+      )}
     </>
   )
 }
